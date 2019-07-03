704ac019e76078b318b797d59c7c75a96c29888e
Nathan <nathan.dearing@digipen.edu>
2019-07-03 13:54:51 -0700

    Added more scenes

---
+++ Arkanoid/Assets/Scripts/ChangeSceneButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneButton : MonoBehaviour
+{
+    
+
+    // Update is called once per 
+    public void onClick()
+    {
+        SceneManager.LoadScene("Gameplay");
+    } 
+}
